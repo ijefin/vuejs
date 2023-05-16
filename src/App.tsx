fec9f79da1869dc066fa93a84b3ee0e9a4023aeb
ijefin <jeffersonmecmiler@hotmail.com>
2023-05-16 15:25:43 -0300

    creating notification page

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="main-card">
+        <div className='card-header'>
+          <div className="header-content">
+            <p className='notifications'>Notifications</p>
+            <p className="notification-amount">3</p>
+          </div>
+          <p className="mark-all">Mark all as read</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
